fa8a80bf24165e70d416efae22bfc859e08e08ed
ian asasira <you@example.com>
2024-10-03 17:58:13 +0300

    Used f string to create Madlibs
    Takes user input stores in a variable and outputs

---
+++ madlibs.py
@@ -0,0 +1,11 @@
+# im going to use the f string to express concatenation
+#print(f"who is the preseident of Uganda{President}")
+
+Prezo = input("Whose you're President:")
+name = input("What's your name:")
+drink = input("What's  Your Fav Drink(Sprite):")
+
+MadlibGame = f"The president of Uganda is chosen by God so it is{Prezo}!\
+    Hey {name}. stay hydrated and drink {drink}"
+
+print(MadlibGame)
